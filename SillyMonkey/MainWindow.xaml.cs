923f5d1b0c721fecc77a687376ce9ce728b0133a
Harlin Zhang <zhanglin65dr@hotmail.com>
2019-09-22 23:10:19 +0800

    update wpf test

--- SillyMonkey/MainWindow.xaml.cs
+++ SillyMonkey/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
@@ -16,6 +17,18 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SillyMonkey {
+    public class OpenFiles{
+        public string File { get; set; }
+        public List<string> Sites { get; set; }
+
+        public OpenFiles(string file) {
+            File = file;
+            Sites = new List<string>();
+            Enumerable.Range(1, 10).ToList().ForEach(x => Sites.Add("Site:" + x));
+        }
+
+    }
+
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
@@ -23,54 +36,16 @@ namespace SillyMonkey {
 
         public MainWindow() {
             InitializeComponent();
-            //Setup();
-            SetupDatatable();
+            Setup();
+            //SetupDatatable();
         }
 
-        void Setup() {
-            Random r = new Random();
+        void Setup(){
+            var data = new ObservableCollection<OpenFiles>();
+            Enumerable.Range(0, 9).ToList().ForEach(x => data.Add(new OpenFiles(x.ToString())));
 
-            int maxCol = 10;
-            List<object> row = new List<object>(maxCol);
-            for(int i=0; i< maxCol; i++) {
-                row.Add(r.NextDouble().ToString());
-            }
-
-            grid.ItemsSource = row;
+            tvFiles.DataContext =data;
 
         }
-        void SetupDatatable() {
-            Stopwatch sp = new Stopwatch();
-
-
-            Random r = new Random();
-
-            int maxCol = 100;
-            int maxRow = 1000;
-            DataTable dataTable = new DataTable();
-            List<object> row = new List<object>(maxCol);
-
-            for (int i = 0; i < maxCol; i++) {
-                dataTable.Columns.Add(i.ToString());
-            }
-
-            for (int i=0; i<maxRow; i++) {
-                for (int j = 0; j < maxCol; j++) {
-                    row.Add(r.NextDouble());
-                }
-                var v = row.ToArray();
-                dataTable.Rows.Add(v);
-                row.Clear();
-            }
-            sp.Start();
-            grid.ItemsSource= dataTable.DefaultView;
-            
-            //grid.UpdateLayout();
-            Console.WriteLine(sp.ElapsedMilliseconds);
-            sp.Stop();
-            sp.Reset();
-
-        }
-
     }
 }
