e46dfbf1a9fdd0e25f36caaf719fd190c4a3cab2
Harlin Zhang <zhanglin65dr@hotmail.com>
2020-05-24 22:39:02 +0800

    add chart

---
+++ SillyMonkeyD/ViewModels/ItemChartTabViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Input;
+using DataInterface;
+using DevExpress.Mvvm;
+using InteractiveDataDisplay.WPF;
+
+namespace SillyMonkeyD.ViewModels {
+    public class ItemChartTabViewModel : ViewModelBase, ITab {
+        public ItemChartTabViewModel(IDataAcquire dataAcquire, int filterId) {
+            Init(dataAcquire, filterId);
+            InitUI();
+        }
+
+        public int FilterId { get; private set; }
+        public IDataAcquire DataAcquire { get; private set; }
+
+        public string TabTitle { get { return GetProperty(() => TabTitle); } private set { SetProperty(() => TabTitle, value); } }
+        public string FilePath { get { return GetProperty(() => FilePath); } private set { SetProperty(() => FilePath, value); } }
+        public int WindowFlag { get; private set; }
+        public DataCollection Data { get { return GetProperty(() => Data); } private set { SetProperty(() => Data, value); } }
+
+        private void Init(IDataAcquire dataAcquire, int filterId) {
+            DataAcquire = dataAcquire;
+            FilterId = filterId;
+            WindowFlag = 2;
+
+            var i = dataAcquire.GetFilterIndex(filterId);
+
+            if (DataAcquire.FileName.Length > 15)
+                TabTitle = DataAcquire.FileName.Substring(0, 15) + "..." + $"-F{i}-RAW";
+            else
+                TabTitle = DataAcquire.FileName + $"-F{i}-RAW";
+            FilePath = DataAcquire.FilePath;
+
+            //Data = new WaferMapGridModel(new WaferMapTable(DataAcquire, FilterId));
+        }
+
+
+        public void UpdateFilter() {
+            
+            RaisePropertyChanged("Data");
+        }
+
+        #region UI
+        public ICommand ExportToExcel { get; private set; }
+
+        private void InitUI() {
+
+        }
+        #endregion
+    }
+}
---
+++ SillyMonkeyD/Views/ItemChartTab.xaml.cs
@@ -0,0 +1,30 @@
+﻿using DataInterface;
+using DevExpress.Xpf.Core;
+using SillyMonkeyD.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SillyMonkeyD.Views {
+    /// <summary>
+    /// Interaction logic for ItemChartTab.xaml
+    /// </summary>
+    public partial class ItemChartTab : DXTabItem {
+        ItemChartTabViewModel itemChartTab;
+        public ItemChartTab(IDataAcquire dataAcquire, int filterId) {
+            InitializeComponent();
+            itemChartTab = new ItemChartTabViewModel(dataAcquire, filterId);
+            DataContext = itemChartTab;
+        }
+    }
+}
