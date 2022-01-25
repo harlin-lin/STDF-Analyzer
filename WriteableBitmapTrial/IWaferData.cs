873439041f3e538ad34a3deb43c7a78f87feada8
zhangling <harlin_zhang@outlook.com>
2022-01-25 18:23:38 +0800

    update wafer tool

--- WriteableBitmapTrial/IWaferData.cs
+++ WriteableBitmapTrial/IWaferData.cs
@@ -8,14 +8,16 @@ namespace WriteableBitmapTrial {
     public class DieInfo {
         public short X { get; }
         public short Y { get; }
+        public short? WaferId { get; }
         public ushort HBin { get; }
         public ushort SBin { get; }
         public byte Site { get; }
         public int Idx { get; }
 
-        public DieInfo(short x, short y, ushort hbin, ushort sbin, byte site, int idx) {
+        public DieInfo(short x, short y, short? waferId,ushort hbin, ushort sbin, byte site, int idx) {
             X = x;
             Y = y;
+            WaferId = waferId;
             HBin = hbin;
             SBin = sbin;
             Site = site;
@@ -23,10 +25,29 @@ namespace WriteableBitmapTrial {
         }
     }
 
+    public enum MapViewMode{
+        Single,
+        Split,
+        Stack
+    }
+
+    public enum MapBinMode {
+        SBin,
+        HBin
+    }
+
+    public enum MapRtDataMode {
+        OverWrite,
+        FirstOnly
+    }
+
     public interface IWaferData {
-        IEnumerable<DieInfo> dieInfoList { get; }
+        IEnumerable<DieInfo> DieInfoList { get; }
         Dictionary<ushort, string> HBinInfo { get; }
         Dictionary<ushort, string> SBinInfo { get; }
-
+        short XUbound { get; }
+        short YUbound { get; }
+        short XLbound { get; }
+        short YLbound { get; }
     }
 }
--- WriteableBitmapTrial/MainWindow.xaml.cs
+++ WriteableBitmapTrial/MainWindow.xaml.cs
@@ -59,13 +59,66 @@ namespace WriteableBitmapTrial {
             get { return false; } 
         }
 
-        private Color[,] _waferMap;
+        private Dictionary<short?, Color[,]> _waferMaps= new Dictionary<short?, Color[,]>();
+        private Dictionary<short?, Color[,]> _rtMaps = new Dictionary<short?, Color[,]>();
+
+        private MapViewMode _mapViewMode = MapViewMode.Single;
+        private MapBinMode _mapBinMode = MapBinMode.SBin;
+        private MapRtDataMode _mapRtDataMode = MapRtDataMode.OverWrite;
+
+        private Dictionary<ushort, int> _sBinIdx= null;
+        private Dictionary<ushort, int> _hBinIdx = null;
+
+        private List<string> _logList = new List<string>();
+
+        private short? _selectedSingleWafer=null;
+
+        private IWaferData _waferData;
+
         private int _xCnt, _yCnt;
-        
 
         private void OnDataSourceChanged(IWaferData waferData) {
+            _waferData = waferData;
 
+            _logList.Clear();
+
+            _hBinIdx = new Dictionary<ushort, int>();
+            _sBinIdx = new Dictionary<ushort, int>();
+
+            _xCnt = waferData.XUbound - waferData.XLbound + 1;
+            _yCnt = waferData.YUbound -waferData.YLbound + 1;
+
+            foreach (var die in waferData.DieInfoList) {
+                if (!_hBinIdx.ContainsKey(die.SBin)) _hBinIdx.Add(die.SBin, _hBinIdx.Count - 1);
+                if (!_sBinIdx.ContainsKey(die.HBin)) _sBinIdx.Add(die.HBin, _hBinIdx.Count - 1);
+
+                if (!_waferMaps.ContainsKey(die.WaferId)) {
+                    _waferMaps.Add(die.WaferId, new Color[_xCnt, _yCnt]);
+                    _rtMaps.Add(die.WaferId, new Color[_xCnt, _yCnt]);
+                }
+                if (die.X> waferData.XUbound || die.X < waferData.XLbound || die.Y> waferData.YUbound || die.Y < waferData.XLbound) {
+                    _logList.Add($"Cord X:{die.X} Y:{die.Y} out of wafer");
+                    continue;
+                }
+                if(_mapRtDataMode == MapRtDataMode.OverWrite || _waferMaps[die.WaferId][die.X, die.Y] == new Color()) {
+                    if(_mapBinMode == MapBinMode.HBin) {
+                        _waferMaps[die.WaferId][die.X, die.Y] = BinColor.GetFailBinColor(_hBinIdx[die.HBin]);
+                    } else {
+                        _waferMaps[die.WaferId][die.X, die.Y] = BinColor.GetFailBinColor(_sBinIdx[die.SBin]);
+                    }
+                } else {
+                    if (_mapBinMode == MapBinMode.HBin) {
+                        _rtMaps[die.WaferId][die.X, die.Y] = BinColor.GetFailBinColor(_hBinIdx[die.HBin]);
+                    } else {
+                        _rtMaps[die.WaferId][die.X, die.Y] = BinColor.GetFailBinColor(_sBinIdx[die.SBin]);
+                    }
+                }
+
+            }
+            
         }
+
+
 
     }
 }
