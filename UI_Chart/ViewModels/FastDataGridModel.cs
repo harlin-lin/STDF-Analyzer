00f67c4d6fb0e037e2feddd64b674b8b1393e94e
harlin <harlin_zhang@outlook.com>
2022-08-21 20:27:58 +0800

    update raw data

---
+++ UI_Chart/ViewModels/FastDataGridModel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using DataContainer;
+using FastWpfGrid;
+
+namespace UI_Chart.ViewModels {
+    public class FastDataGridModel : FastGridModelBase{
+        private IDataAcquire _da;
+        private SubData _subData;
+
+        private Color? _cellColor;
+
+        public FastDataGridModel(SubData subData) {
+            _subData = subData;
+            _da = StdDB.GetDataAcquire(subData.StdFilePath);
+        }
+
+        public override HashSet<int> GetFrozenColumns(IFastGridView view) {
+            var col = new HashSet<int>();
+            col.Add(0);
+            col.Add(1);
+            return col;
+        }
+
+        public override string GetColumnHeaderText(int column) {
+            return (column+1).ToString();
+        }
+
+        public override string GetRowHeaderText(int row) {
+            return string.Empty;
+        }
+
+        public override int ColumnCount {
+            get { return _da.GetFilteredChipsCount(_subData.FilterId) + 2; }
+        }
+
+        public override int RowCount {
+            get { return _da.GetTestIDs().Count() + 6; }
+        }
+
+        public override Color? FontColor {
+            get { return _cellColor; }
+        }
+
+        public override string GetCellText(int row, int column) {
+            _cellColor = null;
+
+            if (column == 0) {
+                if (row < 6) {
+                    return string.Empty;
+                }
+                return _da.GetTestIDs().ElementAt(row-6);
+            } else if (column == 1){
+                if (row == 0) return "Index";
+                if (row == 1) return "Cord";
+                if (row == 2) return "Time";
+                if (row == 3) return "HBin";
+                if (row == 4) return "SBin";
+                if (row == 5) return "Site";
+
+                var uid = _da.GetTestIDs().ElementAt(row-6);
+                return _da.GetTestIDs_Info()[uid].TestText;
+
+            } else if(column > 1){
+                var idx = _da.GetFilteredPartIndex(_subData.FilterId).ElementAt(column - 2);
+                if (row == 0) return idx.ToString();
+                if (row == 1) return _da.GetWaferCord(idx);
+                if (row == 2) return _da.GetTestTime(idx).ToString();
+                if (row == 3) return _da.GetHardBin(idx).ToString();
+                if (row == 4) return _da.GetSoftBin(idx).ToString();
+                if (row == 5) return _da.GetSite(idx).ToString();
+                
+                var uid = _da.GetTestIDs().ElementAt(row - 6);
+                var val = _da.GetItemData(uid, idx);
+                var limit = _da.GetTestInfo(uid);
+
+                if(limit.LoLimit.HasValue && val < limit.LoLimit)
+                    _cellColor = Colors.Blue;
+                else if(limit.HiLimit.HasValue && val > limit.HiLimit)
+                    _cellColor = Colors.Red;
+
+                return val.ToString();
+            }
+            return string.Empty;
+        }
+
+        //public override void SetCellText(int row, int column, string value) {
+        //    var key = Tuple.Create(row, column);
+        //    _editedCells[key] = value;
+        //}
+
+        //public override IFastGridCell GetGridHeader(IFastGridView view) {
+        //    var impl = new FastGridCellImpl();
+        //    var btn = impl.AddImageBlock(view.IsTransposed ? "/Images/flip_horizontal_small.png" : "/Images/flip_vertical_small.png");
+        //    btn.CommandParameter = FastWpfGrid.FastGridControl.ToggleTransposedCommand;
+        //    btn.ToolTip = "Swap rows and columns";
+        //    impl.AddImageBlock("/Images/foreign_keysmall.png").CommandParameter = "FK";
+        //    impl.AddImageBlock("/Images/primary_keysmall.png").CommandParameter = "PK";
+        //    return impl;
+        //}
+
+        //public override void HandleCommand(IFastGridView view, FastGridCellAddress address, object commandParameter, ref bool handled) {
+        //    //base.HandleCommand(view, address, commandParameter, ref handled);
+        //    //if (commandParameter is string) MessageBox.Show(commandParameter.ToString());
+        //}
+
+        //public override int? SelectedRowCountLimit {
+        //    get { return 100; }
+        //}
+
+        //public override void HandleSelectionCommand(IFastGridView view, string command) {
+        //    //MessageBox.Show(command);
+        //}
+
+
+    }
+}
--- UI_Chart/ViewModels/RawViewModel.cs
+++ UI_Chart/ViewModels/RawViewModel.cs
@@ -7,6 +7,9 @@ using SillyMonkey.Core;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using FastWpfGrid;
+using System.Collections.Generic;
+using System;
 
 namespace UI_Chart.ViewModels {
     public class RawViewModel : BindableBase, INavigationAware {
@@ -15,105 +18,105 @@ namespace UI_Chart.ViewModels {
 
         SubData _subData;
 
-        const int CNT_PER_PAGE = 10;
-        private int totalPartCnt = 0;
-        private int totalPages = 0;
-        private int currPage = 0;
+        //const int CNT_PER_PAGE = 10;
+        //private int totalPartCnt = 0;
+        //private int totalPages = 0;
+        //private int currPage = 0;
 
         public RawViewModel(IRegionManager regionManager, IEventAggregator ea) {
             _regionManager = regionManager;
             _ea = ea;
             _ea.GetEvent<Event_FilterUpdated>().Subscribe(x => {
                 if (_subData.Equals(x)) {
-                    currPage = 0;
-                    UpdateTable();
+                    _rawDataModel.NotifyRefresh();
                 }
             });
 
         }
 
-        private void InitUI() {
-            dt.Columns.Add("Idx");
-            dt.Columns[0].DataType = typeof(int);
-            dt.Columns.Add("TestNumber");
-            for (int i = 0; i < CNT_PER_PAGE; i++) {
-                dt.Columns.Add($"{i}");
-            }
-        }
+        //private void InitUI() {
+        //    dt.Columns.Add("Idx");
+        //    dt.Columns[0].DataType = typeof(int);
+        //    dt.Columns.Add("TestNumber");
+        //    for (int i = 0; i < CNT_PER_PAGE; i++) {
+        //        dt.Columns.Add($"{i}");
+        //    }
+        //}
 
-        private void UpdateTable() {
-            dt.Clear();
+        //private void UpdateTable() {
+        //    dt.Clear();
 
-            var da = StdDB.GetDataAcquire(_subData.StdFilePath);
+        //    var da = StdDB.GetDataAcquire(_subData.StdFilePath);
 
-            totalPartCnt = da.GetFilteredChipsCount(_subData.FilterId);
-            totalPages = (totalPartCnt / CNT_PER_PAGE) + ((totalPartCnt % CNT_PER_PAGE) > 0 ? 1 : 0);
-            PageCnt = $"{currPage} / {totalPages - 1}";
-            JumpPage = $"{currPage}";
+        //    totalPartCnt = da.GetFilteredChipsCount(_subData.FilterId);
+        //    totalPages = (totalPartCnt / CNT_PER_PAGE) + ((totalPartCnt % CNT_PER_PAGE) > 0 ? 1 : 0);
+        //    PageCnt = $"{currPage} / {totalPages - 1}";
+        //    JumpPage = $"{currPage}";
 
-            //add column
-            var offset = currPage * CNT_PER_PAGE;
-            var viewCnt = totalPartCnt > (offset + CNT_PER_PAGE) ? CNT_PER_PAGE : totalPartCnt - offset;
-            DataRow r, r_cord, r_time, r_hbin, r_sbin, r_site;
+        //    //add column
+        //    var offset = currPage * CNT_PER_PAGE;
+        //    var viewCnt = totalPartCnt > (offset + CNT_PER_PAGE) ? CNT_PER_PAGE : totalPartCnt - offset;
+        //    DataRow r, r_cord, r_time, r_hbin, r_sbin, r_site;
 
-            r = dt.NewRow();
-            r_cord = dt.NewRow();
-            r_time = dt.NewRow();
-            r_hbin = dt.NewRow();
-            r_sbin = dt.NewRow();
-            r_site = dt.NewRow();
+        //    r = dt.NewRow();
+        //    r_cord = dt.NewRow();
+        //    r_time = dt.NewRow();
+        //    r_hbin = dt.NewRow();
+        //    r_sbin = dt.NewRow();
+        //    r_site = dt.NewRow();
 
-            r[0] = 0;
-            r_cord[0] = 0;
-            r_time[0] = 0;
-            r_hbin[0] = 0;
-            r_sbin[0] = 0;
-            r_site[0] = 0;
+        //    r[0] = 0;
+        //    r_cord[0] = 0;
+        //    r_time[0] = 0;
+        //    r_hbin[0] = 0;
+        //    r_sbin[0] = 0;
+        //    r_site[0] = 0;
 
-            r[1] = "PartIdx";
-            r_cord[1] = "Cord";
-            r_time[1] = "Time";
-            r_hbin[1] = "HBin";
-            r_sbin[1] = "SBin";
-            r_site[1] = "Site";
+        //    r[1] = "PartIdx";
+        //    r_cord[1] = "Cord";
+        //    r_time[1] = "Time";
+        //    r_hbin[1] = "HBin";
+        //    r_sbin[1] = "SBin";
+        //    r_site[1] = "Site";
 
-            int i = 2;
-            foreach (var c in da.GetFilteredPartIndex(_subData.FilterId).Skip(offset).Take(viewCnt)) {
-                r[i] = c;
-                r_cord[i] = da.GetWaferCord(c);
-                r_time[i] = da.GetTestTime(c);
-                r_hbin[i] = da.GetHardBin(c);
-                r_sbin[i] = da.GetSoftBin(c);
-                r_site[i] = da.GetSite(c);
-                i++;
-            }
-            dt.Rows.Add(r);
-            dt.Rows.Add(r_cord);
-            dt.Rows.Add(r_time);
-            dt.Rows.Add(r_hbin);
-            dt.Rows.Add(r_sbin);
-            dt.Rows.Add(r_site);
+        //    int i = 2;
+        //    foreach (var c in da.GetFilteredPartIndex(_subData.FilterId).Skip(offset).Take(viewCnt)) {
+        //        r[i] = c;
+        //        r_cord[i] = da.GetWaferCord(c);
+        //        r_time[i] = da.GetTestTime(c);
+        //        r_hbin[i] = da.GetHardBin(c);
+        //        r_sbin[i] = da.GetSoftBin(c);
+        //        r_site[i] = da.GetSite(c);
+        //        i++;
+        //    }
+        //    dt.Rows.Add(r);
+        //    dt.Rows.Add(r_cord);
+        //    dt.Rows.Add(r_time);
+        //    dt.Rows.Add(r_hbin);
+        //    dt.Rows.Add(r_sbin);
+        //    dt.Rows.Add(r_site);
 
-            int idx = 1;
-            foreach (var uid in da.GetTestIDs()) {
-                r = dt.NewRow();
-                r[0] = idx++;
-                r[1] = uid;
-                i = 2;
-                foreach (var v in da.GetFilteredItemData(uid, _subData.FilterId).Skip(offset).Take(viewCnt)) {
-                    r[i++] = v;
-                }
-                dt.Rows.Add(r);
-            }
-            RaisePropertyChanged("TestItems");
-        }
+        //    int idx = 1;
+        //    foreach (var uid in da.GetTestIDs()) {
+        //        r = dt.NewRow();
+        //        r[0] = idx++;
+        //        r[1] = uid;
+        //        i = 2;
+        //        foreach (var v in da.GetFilteredItemData(uid, _subData.FilterId).Skip(offset).Take(viewCnt)) {
+        //            r[i++] = v;
+        //        }
+        //        dt.Rows.Add(r);
+        //    }
+        //    RaisePropertyChanged("TestItems");
+        //}
 
         public void OnNavigatedTo(NavigationContext navigationContext) {
             var data = (SubData)navigationContext.Parameters["subData"];
             if (!_subData.Equals(data)) {
                 _subData = data;
-                InitUI();
-                UpdateTable();
+                RawDataModel = new FastDataGridModel(_subData);
+                //InitUI();
+                //UpdateTable();
             }
         }
 
@@ -128,80 +131,88 @@ namespace UI_Chart.ViewModels {
         }
 
 
-        private DataTable dt = new DataTable();
-        public DataTable TestItems {
-            get { return dt; }
-            set { SetProperty(ref dt, value); }
+        //private DataTable dt = new DataTable();
+        //public DataTable TestItems {
+        //    get { return dt; }
+        //    set { SetProperty(ref dt, value); }
+        //}
+
+
+        //private string _pageCount;
+        //public string PageCnt {
+        //    get { return _pageCount; }
+        //    set { SetProperty(ref _pageCount, value); }
+        //}
+
+        //private string _jumpPage;
+        //public string JumpPage {
+        //    get { return _jumpPage; }
+        //    set { SetProperty(ref _jumpPage, value); }
+        //}
+
+        //private DelegateCommand jumpFirstPage;
+        //public DelegateCommand JumpFirstPage =>
+        //    jumpFirstPage ?? (jumpFirstPage = new DelegateCommand(ExecuteJumpFirstPage));
+
+        //void ExecuteJumpFirstPage() {
+        //    currPage = 0;
+        //    UpdateTable();
+        //}
+
+        //private DelegateCommand jumpPreviousPage;
+        //public DelegateCommand JumpPreviousPage =>
+        //    jumpPreviousPage ?? (jumpPreviousPage = new DelegateCommand(ExecuteJumpPreviousPage));
+
+        //void ExecuteJumpPreviousPage() {
+        //    if (currPage > 0) {
+        //        currPage--;
+        //        UpdateTable();
+        //    }
+        //}
+
+        //private DelegateCommand jumpNextPage;
+        //public DelegateCommand JumpNextPage =>
+        //    jumpNextPage ?? (jumpNextPage = new DelegateCommand(ExecuteJumpNextPage));
+
+        //void ExecuteJumpNextPage() {
+        //    if (currPage < (totalPages - 1)) {
+        //        currPage++;
+        //        UpdateTable();
+        //    }
+        //}
+
+        //private DelegateCommand jumpLastPage;
+        //public DelegateCommand JumpLastPage =>
+        //    jumpLastPage ?? (jumpLastPage = new DelegateCommand(ExecuteJumpLastPage));
+
+        //void ExecuteJumpLastPage() {
+        //    currPage = totalPages - 1;
+        //    UpdateTable();
+        //}
+
+        //private DelegateCommand _jumpSelectedPage;
+        //public DelegateCommand JumpSelectedPage =>
+        //    _jumpSelectedPage ?? (_jumpSelectedPage = new DelegateCommand(ExecuteJumpSelectedPage));
+
+        //void ExecuteJumpSelectedPage() {
+        //    int targetPage = 0;
+        //    if (int.TryParse(JumpPage, out targetPage)) {
+        //        if (targetPage <= (totalPages - 1) && targetPage >= 0) {
+        //            currPage = targetPage;
+        //            UpdateTable();
+        //        }
+        //    } else {
+        //        MessageBox.Show("Wrong target box");
+        //    }
+        //}
+
+
+        private FastGridModelBase _rawDataModel;
+        public FastGridModelBase RawDataModel {
+            get { return _rawDataModel; }
+            set { SetProperty(ref _rawDataModel, value); }
         }
 
-
-        private string _pageCount;
-        public string PageCnt {
-            get { return _pageCount; }
-            set { SetProperty(ref _pageCount, value); }
-        }
-
-        private string _jumpPage;
-        public string JumpPage {
-            get { return _jumpPage; }
-            set { SetProperty(ref _jumpPage, value); }
-        }
-
-        private DelegateCommand jumpFirstPage;
-        public DelegateCommand JumpFirstPage =>
-            jumpFirstPage ?? (jumpFirstPage = new DelegateCommand(ExecuteJumpFirstPage));
-
-        void ExecuteJumpFirstPage() {
-            currPage = 0;
-            UpdateTable();
-        }
-
-        private DelegateCommand jumpPreviousPage;
-        public DelegateCommand JumpPreviousPage =>
-            jumpPreviousPage ?? (jumpPreviousPage = new DelegateCommand(ExecuteJumpPreviousPage));
-
-        void ExecuteJumpPreviousPage() {
-            if (currPage > 0) {
-                currPage--;
-                UpdateTable();
-            }
-        }
-
-        private DelegateCommand jumpNextPage;
-        public DelegateCommand JumpNextPage =>
-            jumpNextPage ?? (jumpNextPage = new DelegateCommand(ExecuteJumpNextPage));
-
-        void ExecuteJumpNextPage() {
-            if (currPage < (totalPages - 1)) {
-                currPage++;
-                UpdateTable();
-            }
-        }
-
-        private DelegateCommand jumpLastPage;
-        public DelegateCommand JumpLastPage =>
-            jumpLastPage ?? (jumpLastPage = new DelegateCommand(ExecuteJumpLastPage));
-
-        void ExecuteJumpLastPage() {
-            currPage = totalPages - 1;
-            UpdateTable();
-        }
-
-        private DelegateCommand _jumpSelectedPage;
-        public DelegateCommand JumpSelectedPage =>
-            _jumpSelectedPage ?? (_jumpSelectedPage = new DelegateCommand(ExecuteJumpSelectedPage));
-
-        void ExecuteJumpSelectedPage() {
-            int targetPage = 0;
-            if (int.TryParse(JumpPage, out targetPage)) {
-                if (targetPage <= (totalPages - 1) && targetPage >= 0) {
-                    currPage = targetPage;
-                    UpdateTable();
-                }
-            } else {
-                MessageBox.Show("Wrong target box");
-            }
-        }
 
     }
 }
